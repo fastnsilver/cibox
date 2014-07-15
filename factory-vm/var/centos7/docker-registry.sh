a6c1ffed426d504602427a7600121d15430a9c19
Chris Phillipson <fastnsilver@gmail.com>
2014-07-15 09:11:08 -0700

    Favor docker hub for sourcing CentOS

--- factory-vm/var/centos7/docker-registry.sh
+++ factory-vm/var/centos7/docker-registry.sh
@@ -16,6 +16,4 @@ systemctl start docker-registry
 systemctl status docker-registry
 
 # Seed registry with CentOS 7.0 Docker image
-# from http://szlevi.com/2014/07/03/getting-docker-up-and-running-on-centos-7-beta/
-wget http://buildlogs.centos.org/centos/7/docker/CentOS-7-20140625-x86_64-docker_01.img.tar.xz
-docker import -< CentOS-7-20140625-x86_64-docker_01.img.tar.xz
+docker pull centos:centos7
