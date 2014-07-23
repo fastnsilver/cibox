bc454b4b35640d2f174da4fb683a145fa703f0f8
Chris Phillipson <fastnsilver@gmail.com>
2014-07-23 06:44:41 -0700

    Incorporate Git, Docker, Debootstrap and OS-flavor docker base container into base-vm.  Remove from factory-vm.

--- base-vm/centos-7.0-x64/scripts/setup.sh
+++ base-vm/centos-7.0-x64/scripts/setup.sh
@@ -2,8 +2,41 @@
 
 trap 'exit' ERR
 
-# Get and install EPEL
+# Set local variables
 VAGRANT_HOME=/home/vagrant
+GIT_VERSION=2.0.1
+
+
+# Get and install EPEL
 wget http://mirrors.mit.edu/epel/beta/7/x86_64/epel-release-7-0.2.noarch.rpm
 chown vagrant:vagrant $VAGRANT_HOME/epel-release-7-0.2.noarch.rpm
 yum -y install epel-release-7-0.2.noarch.rpm
+
+
+# Install Git
+yum -y install curl-devel expat-devel gettext-devel perl-ExtUtils-MakeMaker
+wget https://www.kernel.org/pub/software/scm/git/git-$GIT_VERSION.tar.gz
+chown vagrant:vagrant $VAGRANT_HOME/git-$VERSION.tar.gz
+tar xzf git-$GIT_VERSION.tar.gz
+cd git-$GIT_VERSION
+make prefix=/usr/local/git all
+make prefix=/usr/local/git install
+echo "export PATH=/usr/local/git/bin:$PATH" >> /etc/bashrc
+source /etc/bashrc
+
+
+# Install debootstrap (for building Docker base containers)
+yum install -y debootstrap
+
+
+# Install docker
+yum install -y docker-io
+systemctl enable docker.service
+echo -e 'DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock"\n' > /etc/default/docker
+systemctl start docker.service
+systemctl status docker.service
+usermod -a -G docker vagrant
+
+
+# Pull Centos 7 container
+docker pull centos:centos7
