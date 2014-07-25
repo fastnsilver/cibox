fd126b87062af33d69c9feba572032177b376ae0
Chris Phillipson <fastnsilver@gmail.com>
2014-07-25 13:40:46 -0700

    Initialize RHEL 7 with what was learned from CentOS 7. And attempt to bootstrap supervisord.conf.

--- factory-vm/rhel-7.0-x64/containers/base/launch.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Post install commands
-# Note: data directory will be /var/lib/pgsql/9.4/data
-/usr/pgsql-9.4/bin/initdb
-
-# Start PostgreSQL automatically on OS startup
-systemctl enable postgresql-9.4.service
-systemctl start postgresql-9.4.service
