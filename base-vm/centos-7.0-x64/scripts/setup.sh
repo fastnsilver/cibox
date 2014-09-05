16f05494c42832506d5a4072d23ff281b448c1f1
Chris Phillipson <fastnsilver@gmail.com>
2014-09-05 16:12:45 -0700

    Tell yum to install Docker 1.2.0 anyway b/c we already upgraded from source systemd to 216.

--- base-vm/centos-7.0-x64/scripts/setup.sh
+++ base-vm/centos-7.0-x64/scripts/setup.sh
@@ -36,7 +36,7 @@ DOCKER_OS=2.fc20
 DOCKER_ARCH=x86_64
 wget --retry-connrefused http://kojipkgs.fedoraproject.org/packages/docker-io/$DOCKER_VERSION/$DOCKER_OS/$DOCKER_ARCH/docker-io-$DOCKER_VERSION-$DOCKER_OS.$DOCKER_ARCH.rpm
 chown vagrant:vagrant $VAGRANT_HOME/docker-io-$DOCKER_VERSION-$DOCKER_OS.$DOCKER_ARCH.rpm
-yum -y localinstall docker-io-$DOCKER_VERSION-$DOCKER_OS.$DOCKER_ARCH.rpm
+yum -y localinstall --skip-broken docker-io-$DOCKER_VERSION-$DOCKER_OS.$DOCKER_ARCH.rpm
 systemctl enable docker.service
 echo -e 'DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock"\n' > /etc/sysconfig/docker
 systemctl start docker.service
