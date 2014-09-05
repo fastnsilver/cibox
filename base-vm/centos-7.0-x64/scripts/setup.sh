eea11e2fb9ac6be2f5b584aa625d4eccff109244
Chris Phillipson <fastnsilver@gmail.com>
2014-09-05 14:40:32 -0700

    Try a different package based on FC20.  Back out libvirt for lxc.

--- base-vm/centos-7.0-x64/scripts/setup.sh
+++ base-vm/centos-7.0-x64/scripts/setup.sh
@@ -8,7 +8,7 @@ GIT_VERSION=2.1.0
 
 # Get and install Git
 yum install -y curl gcc "kernel-devel-$(uname -r)" kernel-devel kernel-headers \
-	curl-devel expat-devel gettext-devel libvirt libvirt-client python-virtinst openssl-devel zlib-devel perl-ExtUtils-MakeMaker
+	curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-ExtUtils-MakeMaker
 wget --retry-connrefused https://www.kernel.org/pub/software/scm/git/git-$GIT_VERSION.tar.gz
 chown vagrant:vagrant $VAGRANT_HOME/git-$GIT_VERSION.tar.gz
 tar xzf git-$GIT_VERSION.tar.gz
@@ -22,10 +22,11 @@ rm -rf $VAGRANT_HOME/git-$GIT_VERSION*
 # Install docker
 cd $VAGRANT_HOME
 DOCKER_VERSION=1.2.0
-DOCKER_ARCH=1.el6.x86_64
-wget --retry-connrefused http://kojipkgs.fedoraproject.org/packages/docker-io/$DOCKER_VERSION/1.el6/x86_64/docker-io-$DOCKER_VERSION-$DOCKER_ARCH.rpm
-chown vagrant:vagrant $VAGRANT_HOME/docker-io-$DOCKER_VERSION-$DOCKER_ARCH.rpm
-yum -y localinstall docker-io-$DOCKER_VERSION-$DOCKER_ARCH.rpm
+DOCKER_OS=1.fc20
+DOCKER_ARCH=x86_64
+wget --retry-connrefused http://kojipkgs.fedoraproject.org/packages/docker-io/$DOCKER_VERSION/$DOCKER_OS/$DOCKER_ARCH/docker-io-$DOCKER_VERSION-$DOCKER_OS.$DOCKER_ARCH.rpm
+chown vagrant:vagrant $VAGRANT_HOME/docker-io-$DOCKER_VERSION-$DOCKER_OS.$DOCKER_ARCH.rpm
+yum -y localinstall docker-io-$DOCKER_VERSION-$DOCKER_OS.$DOCKER_ARCH.rpm
 systemctl enable docker.service
 echo -e 'DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock"\n' > /etc/sysconfig/docker
 systemctl start docker.service
